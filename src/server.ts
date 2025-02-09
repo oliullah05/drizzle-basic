3a0cc0a90cde4b273b2c90d1270a8a044baf50c9
oliullah05 <mdolihasan@gmail.com>
2025-02-09 22:42:44 +0600

    feat: insert to mySql db

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/mysql2';
+import { usersTable } from '../schema';
+
+
+const db = drizzle(process.env.DATABASE_URL!, { logger: true });
+async function main() {
+try{
+    const userCreate = await db.insert(usersTable).values({
+        name: "Oliullah",
+        age: 50,
+        email: "mdolihasan@gmail.com"
+    })
+
+
+    const userFind = await db.select().from(usersTable);
+}
+
+catch(err:any){
+    console.log({err:err?.message});
+} 
+
+
+    // console.log({userCreate,userFind});
+}
+main();
